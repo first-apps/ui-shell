22063b16bc23b4c43ab1270250c616be1b2d127a
dikshit-n <62920150+dikshit-n@users.noreply.github.com>
2025-01-04 12:38:09 +0530

    @first-apps/shared/components integration

---
+++ app/login/page.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import Link from "next/link";
+import { Button } from "@first-apps/shared/components";
+
+export default function Login() {
+  const handleLogin = () => {
+    // router.replace("/creator");
+  };
+
+  return (
+    <>
+      <Button onClick={handleLogin}>Login</Button>
+      <Button>Get Session</Button>
+      <Link href="/signup">Go to Signup</Link>
+    </>
+  );
+}
